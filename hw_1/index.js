d95863ecef727aa618df453badc521e615ffa9df
Pavel Karpenko <ospkiev@gmail.com>
2025-06-17 21:05:08 +0300

    Add README and allow PORT to configure server

--- hw_1/index.js
+++ hw_1/index.js
@@ -9,6 +9,8 @@ const server = http.createServer((req, res) => {
   res.writeHead(404).end('Not found');
 });
 
-server.listen(3000, () => {
-  console.log('🚀  http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🚀  http://localhost:${PORT}`);
 });
