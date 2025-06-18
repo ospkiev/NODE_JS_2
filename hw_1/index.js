3838598b09efae244192a57b973981c089ad1ab7
Pavel Karpenko <ospkiev@gmail.com>
2025-06-18 12:17:10 +0300

    add_eslint

--- hw_1/index.js
+++ hw_1/index.js
@@ -1,14 +1,17 @@
 import http from 'node:http';
-import { handle as userCtrl } from './controllers/habits.controller.js';
+import { handle as habitsCtrl } from './controllers/habits.controller.js';
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  console.log('first')
   const [, resource, id] = req.url.split('/');
   if (resource === 'habits') {
-    return userCtrl(req, res, id || null);
+    return habitsCtrl(req, res, id ? Number(id) : null);
   }
   res.writeHead(404).end('Not found');
 });
 
-server.listen(3000, () => {
-  console.log('🚀  http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
